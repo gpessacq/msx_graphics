580c6a87eedb69b1540a8a22d3900ecb5a6ba41b
Gustavo Pessacq <gustavo.pessacq@gmail.com>
2016-04-13 11:12:39 -0300

    Initialize canvas with init method

--- msx_graphics.js
+++ msx_graphics.js
@@ -1,12 +1,18 @@
+var msxGraphics = (function(){
 
-function line(x1, y1, x2, y2, color) {
+  var context = null;
 
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2);
-	ctx.strokeStyle = color;
-	ctx.stroke();
-}
+  return {
+    init : function(ctx){
+      context = ctx;
+    },
+    line : function(x1, y1, x2, y2, color){
+		context.beginPath();
+		context.moveTo(x1, y1);
+		context.lineTo(x2, y2);
+		context.strokeStyle = color;
+		context.stroke();
+    }
+  }
 
+})(); 
