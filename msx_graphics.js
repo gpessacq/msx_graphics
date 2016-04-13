073b0d6d90e5ddd979fdcbf4378a1ddc8c1820ae
Gustavo Pessacq <gustavo.pessacq@gmail.com>
2016-04-13 14:19:28 -0300

    Added fill square option

--- msx_graphics.js
+++ msx_graphics.js
@@ -1,6 +1,7 @@
 var msxGraphics = (function(){
 
   var context = null;
+  var color = null;
 
   return {
   	
@@ -13,12 +14,22 @@ var msxGraphics = (function(){
        	context.fillRect(x1,y1,1,1);
   	},
   	
-    line : function(x1, y1, x2, y2, color){
-		context.beginPath();
-		context.moveTo(x1, y1);
-		context.lineTo(x2, y2);
-		context.strokeStyle = color;
-		context.stroke();
+    line : function(x1, y1, x2, y2, color, box_full){
+    	if (box_full){
+    		var x3, y3;
+    		x3 = x2 - x1;
+    		y3 = y2 - y1;
+    		context.fillStyle = color;
+    		context.fillRect(x1, y1, x3, y3);
+    		
+    	}
+    	else{
+			context.beginPath();
+			context.moveTo(x1, y1);
+			context.lineTo(x2, y2);
+			context.strokeStyle = color;
+			context.stroke();
+		}
     },
     circle : function(x1, y1, radius, color){
 		context.beginPath();
