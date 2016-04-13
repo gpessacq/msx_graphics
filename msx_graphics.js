9ddba94d63b086e324bf01bf4e941b8e72b5984b
Gustavo Pessacq <gustavo.pessacq@gmail.com>
2016-04-13 12:40:41 -0300

    Added pset method

--- msx_graphics.js
+++ msx_graphics.js
@@ -3,9 +3,16 @@ var msxGraphics = (function(){
   var context = null;
 
   return {
+  	
     init : function(ctx){
       context = ctx;
     },
+    
+    pset : function(x1, y1, color){
+    	context.fillStyle = color;
+       	context.fillRect(x1,y1,1,1);
+  	},
+  	
     line : function(x1, y1, x2, y2, color){
 		context.beginPath();
 		context.moveTo(x1, y1);
@@ -13,6 +20,7 @@ var msxGraphics = (function(){
 		context.strokeStyle = color;
 		context.stroke();
     }
+
   }
 
 })(); 
